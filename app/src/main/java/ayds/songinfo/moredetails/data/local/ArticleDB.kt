117f325f0e9f3a7d012cddc8d255966395a4241d
GonzaloMontenegro <gonmontegon@hotmail.com>
2024-05-27 17:15:15 -0300

    Clean MVP
    se creo Data,Domain,presentation,injector

--- app/src/main/java/ayds/songinfo/moredetails/data/local/ArticleDB.kt
+++ app/src/main/java/ayds/songinfo/moredetails/data/local/ArticleDB.kt
@@ -1,4 +1,4 @@
-package ayds.songinfo.moredetails.fulllogic
+package ayds.songinfo.moredetails.data.local
 
 import androidx.room.Dao
 import androidx.room.Database
@@ -8,27 +8,21 @@ import androidx.room.OnConflictStrategy
 import androidx.room.PrimaryKey
 import androidx.room.Query
 import androidx.room.RoomDatabase
-
 @Database(entities = [ArticleEntity::class], version = 1)
 abstract class ArticleDatabase : RoomDatabase() {
     abstract fun ArticleDao(): ArticleDao
 }
-
 @Entity
 data class ArticleEntity(
-    @PrimaryKey
-    val artistName: String,
-    val biography: String,
-    val articleUrl: String,
+        @PrimaryKey
+        val artistName: String,
+        val biography: String,
+        val articleUrl: String,
 )
-
 @Dao
 interface ArticleDao {
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertArticle(article: ArticleEntity)
-
     @Query("SELECT * FROM Articleentity WHERE artistName LIKE :artistName LIMIT 1")
     fun getArticleByArtistName(artistName: String): ArticleEntity?
-
 }
